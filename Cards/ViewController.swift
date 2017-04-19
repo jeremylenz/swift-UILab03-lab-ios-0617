164452375770150de16a9e240452616c69c8ac41
jeremylenz <jeremylenz@gmail.com>
2017-04-19 10:09:47 -0400

    i did the cards

--- Cards/ViewController.swift
+++ Cards/ViewController.swift
@@ -16,10 +16,42 @@ class ViewController: UIViewController {
         super.viewDidLoad()
     }
     
-    
+    @IBOutlet weak var cardFaceNumber: UILabel!
+    @IBOutlet weak var cardFaceSuitUpperLeft: UILabel!
+    @IBOutlet weak var cardFaceSuitUpperRight: UILabel!
+    // I know this should be lowerRight but it's too much trouble to change the name because it's attached to an IBOutlet and the last time I tried to change a name the app wouldn't execute.
     
     // TODO: IB actions and code to update UI
 
   
+       @IBAction func updateCardFace(_ sender:UIButton)  {
+        let buttonText = sender.currentTitle ?? ""
+        switch buttonText {
+        case "♣️4":
+                cardFaceNumber.text = "4"
+                cardFaceSuitUpperLeft.text = "♣️"
+                cardFaceSuitUpperRight.text = "♣️"
+        case "♠️3":
+            cardFaceNumber.text = "3"
+            cardFaceSuitUpperLeft.text = "♠️"
+            cardFaceSuitUpperRight.text = "♠️"
 
+        case "♦️8":
+            cardFaceNumber.text = "8"
+            cardFaceSuitUpperLeft.text = "♦️"
+            cardFaceSuitUpperRight.text = "♦️"
+
+        case "♥️10":
+            cardFaceNumber.text = "10"
+            cardFaceSuitUpperLeft.text = "♥️"
+            cardFaceSuitUpperRight.text = "♥️"
+        default:
+            cardFaceNumber.text = "--"
+            cardFaceSuitUpperLeft.text = ""
+            cardFaceSuitUpperRight.text = ""
+
+        }
+        
+    }
+    
 }
